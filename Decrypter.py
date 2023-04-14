214ea1ac9d088689327bc0193ff374440846bdc1
B.Fethi <54514676+Whavi@users.noreply.github.com>
2023-04-14 21:16:55 +0200

    Update Decrypter.py

--- Decrypter.py
+++ Decrypter.py
@@ -1,11 +1,11 @@
-message = input("Veuillez donner votre message à décrypter :")
-text = ''
-for char in message:
+text = input('Veuillez donner votre message à décrypter :')
+message = ''
+for char in text:
     if not char.isalpha():
         continue
-    char = char.upper() #mettre en maj le message
-    lettre = ord(char) - 1 #décrementer de 1 les lettres
-    if lettre < ord('Z'): #si le message depasse le A sa revien en Z
-        lettre = ord('A')
-    text += chr(lettre)
-print(text) #Affichage du resultat
+    char = char.upper()
+    lettre = ord(char) - 1
+    if lettre < ord('A'):
+        lettre = ord('Z')
+    message += chr(lettre)
+print(message)
