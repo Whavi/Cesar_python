fe99f28657cb7194bf52c69bdf25345c6e69881a
B.Fethi <54514676+Whavi@users.noreply.github.com>
2023-04-17 14:17:05 +0200

    Delete Palindrome.py

--- Palindrome.py
+++
@@ -1,9 +0,0 @@
-message = input("Veuillez donner un mot ou une phrase palindrome : ") 
-strin = ""
-sansEspace = message.replace(" ", "").lower() #Remplace les espaces par du vide
-sansEspaceReverse = strin.join(reversed(sansEspace)) # inverse la chaine de caractère
-
-if(sansEspace == sansEspaceReverse):  #condition if,else
-    print("Le message est un palindrome")
-else:
-    print("Le message n'est un pas palindrome")
